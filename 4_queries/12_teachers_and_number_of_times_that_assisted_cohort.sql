2f6330abfb18fab2f9da55112213009d144558b2
AnthonyA <alicea.9a@gmail.com>
2025-01-17 14:20:10 -0700

    feat: Add 4_queries folder containing queries using new tables to test against the database

---
+++ 4_queries/12_teachers_and_number_of_times_that_assisted_cohort.sql
@@ -0,0 +1,25 @@
+Select DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(*) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+
+
+
+
+
+Perform the same query as before, but include the number of assistances as well.
+
+Expected Result:
+
+      teacher       | cohort | total_assistances 
+--------------------+--------+-------------------
+ Cheyanne Powlowski | JUL02  |               336
+ Georgiana Fahey    | JUL02  |               158
+ Helmer Rodriguez   | JUL02  |               157
+ Jadyn Bosco        | JUL02  |               177
+...
+(8 rows)
