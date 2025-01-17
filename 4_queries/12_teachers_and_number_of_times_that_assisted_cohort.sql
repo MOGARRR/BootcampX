09b574302657f166ad29318784c682fd761fb4e9
AnthonyA <alicea.9a@gmail.com>
2025-01-17 14:32:59 -0700

    style/refactor: Remove querie parameters from file and revert tables to previous version

--- 4_queries/12_teachers_and_number_of_times_that_assisted_cohort.sql
+++ 4_queries/12_teachers_and_number_of_times_that_assisted_cohort.sql
@@ -6,20 +6,3 @@ JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-
-
-
-
-
-Perform the same query as before, but include the number of assistances as well.
-
-Expected Result:
-
-      teacher       | cohort | total_assistances 
---------------------+--------+-------------------
- Cheyanne Powlowski | JUL02  |               336
- Georgiana Fahey    | JUL02  |               158
- Helmer Rodriguez   | JUL02  |               157
- Jadyn Bosco        | JUL02  |               177
-...
-(8 rows)
